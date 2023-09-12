d2e9b88a5ced9f0a9d9351f15dd39ba2e2ad3e24
Manikanta662001 <gundlurimanikanta142@gmail.com>
2023-09-12 12:27:25 +0530

    -

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -6,6 +6,7 @@ import AddEditContact from './AddEditContact'
 const Contacts = ({contacts,getAllContacts}) => {
   useEffect(()=>{
     getAllContacts()
+    // eslint-disable-next-line
   },[])
    
   return (
